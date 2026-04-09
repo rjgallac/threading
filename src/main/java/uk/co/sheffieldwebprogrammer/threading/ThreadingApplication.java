f669e07ee5debf94047c1203183ce73360b315c0
rjgallac <rjgallac@hotmail.com>
2026-04-09 21:16:32 +0100

    threading init

---
+++ src/main/java/uk/co/sheffieldwebprogrammer/threading/ThreadingApplication.java
@@ -0,0 +1,13 @@
+package uk.co.sheffieldwebprogrammer.threading;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ThreadingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ThreadingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/uk/co/sheffieldwebprogrammer/threading/ThreadingApplicationTests.java
@@ -0,0 +1,13 @@
+package uk.co.sheffieldwebprogrammer.threading;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ThreadingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
