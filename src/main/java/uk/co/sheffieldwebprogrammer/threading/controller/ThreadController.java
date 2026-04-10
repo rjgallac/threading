d7a5cd27a0aae0c99d9c84604fa94808957735c1
rjgallac <rjgallac@hotmail.com>
2026-04-10 19:52:31 +0100

    update jmeter plan

--- src/main/java/uk/co/sheffieldwebprogrammer/threading/controller/ThreadController.java
+++ src/main/java/uk/co/sheffieldwebprogrammer/threading/controller/ThreadController.java
@@ -1,5 +1,6 @@
 package uk.co.sheffieldwebprogrammer.threading.controller;
 
+
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -7,8 +8,11 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/thread")
 public class ThreadController {
+
     @GetMapping("/test")
-    public String test() {
+    public String test() throws InterruptedException {
+        Thread.sleep(1000); // Simulate some work
+        System.out.println("Handled request in thread: " + Thread.currentThread().toString());
         return "Hello from thread: " + Thread.currentThread().getName();
     }
 }
